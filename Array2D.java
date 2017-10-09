074da89f517cbf5b1617252dffe971a96bb8b966
Neil Manimtim <31807992+nman03@users.noreply.github.com>
2017-10-08 19:42:38 -0700

    implemented add methods

--- Array2D.java
+++ Array2D.java
@@ -1,9 +1,9 @@
 package array2d;
 
 public class Array2D<E> {
-	int rows;
-	int cols;
-	Array2DNode<E> head;
+	private int rows;
+	private int cols;
+	private Array2DNode<E> head;
 
 	public Array2D() {
 	}
@@ -91,76 +91,208 @@ public class Array2D<E> {
 	}
 
 	public String toString() {
-		return null;
+		String st = "";
+		Array2DNode<E> current = head;
+		Array2DNode<E> tmp = head;
+		
+		for (int i = 0 ; i < rows ; i++) {
+			for (int j = 0 ; j < cols ; j++) {
+				st += String.valueOf(current.getItem()) + " ";
+				current = current.right;
+			}
+			if (i < rows - 1)
+				tmp = tmp.down;
+			
+			current = tmp;
+			st += "\n";
+		}
+		
+		return st;
 
 	}
 
 	public void addFirstCol() { 
-		// Adds a new column to the beginning of the list. 
+		Array2DNode<E> current = new Array2DNode<>();
+		Array2DNode<E> tmp = head;
+		head = current;
+		cols++;
+		
+		for (int i = 0 ; i < rows ; i++) {
+			if (i < rows - 1) 
+				current.down = new Array2DNode<>();		
+			
+			current.right = tmp;
+			current = current.down;
+			tmp = tmp.down;
+		}	
 	}
 
 	public void addFirstRow() {
-		// Adds a new row to the beginning of the list. 
+		Array2DNode<E> current = new Array2DNode<>();
+		Array2DNode<E> tmp = head;
+		head = current;
+		rows++;
+		
+		for (int i = 0 ; i < cols ; i++) {
+			if (i < cols - 1) 
+				current.right = new Array2DNode<>();		
+			
+			current.down = tmp;
+			current = current.right;
+			tmp = tmp.right;
+		}
 	}
-	
+
 	public void addLastCol() {
-		// Adds a new column to the end of the list. 
+		Array2DNode<E> current = new Array2DNode<>();
+		Array2DNode<E> tmp = head;
+		
+		for (int k = 1 ; k < cols ; k++)
+			tmp = tmp.right; 
+		
+		for (int i = 0 ; i < rows ; i++) {
+			if (i < rows - 1) 
+				current.down = new Array2DNode<>();		
+			
+			tmp.right = current;
+			current = current.down;
+			tmp = tmp.down;
+		}
+		
+		cols++;
 	}
-	
+
 	public void addLastRow() {
-		// Adds a new column to the end of the list. 
+		Array2DNode<E> current = new Array2DNode<>();
+		Array2DNode<E> tmp = head;
+		
+		for (int k = 1 ; k < rows ; k++)
+			tmp = tmp.down; 
+		
+		for (int i = 0 ; i < cols ; i++) {
+			if (i < cols - 1) 
+				current.right = new Array2DNode<>();		
+			
+			tmp.down = current;
+			current = current.right;
+			tmp = tmp.right;
+		}
+		
+		rows++;
 	}
-	
-	public void addCol(int colIndex) {
-		// Inserts a column at the given index. (Insert here means the columns shift over by 1 from the insertion point onward). 
+
+	public void addCol(int colIndex) {	
+		if (colIndex > cols || colIndex < 0)
+			throw new IndexOutOfBoundsException("Out of Bounds");
+		
+		if (colIndex == 0) {
+			addFirstCol();
+		} else if (colIndex == cols) {
+			addLastCol();
+		} else {	
+			Array2DNode<E> current = new Array2DNode<>();
+			Array2DNode<E> prevCol = head;
+			Array2DNode<E> nextCol = head;
+			
+			for (int k = 0 ; k < colIndex ; k++) {
+				if (k < colIndex - 1) 
+					prevCol = prevCol.right;
+				nextCol = nextCol.right;
+			}	
+			
+			for (int i = 0 ; i < rows ; i++) {
+				if (i < rows - 1) 
+					current.down = new Array2DNode<>();		
+				
+				prevCol.right = current;
+				current.right = nextCol;
+				current = current.down;
+				prevCol = prevCol.down;
+				nextCol = nextCol.down;
+			}
+			
+			cols++;
+		}
+				
 	}
-	
+
 	public void addRow(int rowIndex) {
-		// Inserts a row at the given index. (Insert here means the rows shift over by 1 from the insertion point onward). 
+		if (rowIndex > rows || rowIndex < 0)
+			throw new IndexOutOfBoundsException("Out of Bounds");	
+		
+		
+		if (rowIndex == 0) {
+			addFirstRow();
+		} else if (rowIndex == rows) {
+			addLastRow();
+		} else {	
+			Array2DNode<E> current = new Array2DNode<>();
+			Array2DNode<E> prevRow = head;
+			Array2DNode<E> nextRow = head;
+			
+			for (int k = 0 ; k < rowIndex ; k++) {
+				if (k < rowIndex - 1) 
+					prevRow = prevRow.down;
+				nextRow = nextRow.down;
+			}	
+			
+			for (int i = 0 ; i < cols ; i++) {
+				if (i < cols - 1) 
+					current.right = new Array2DNode<>();		
+				
+				prevRow.down = current;
+				current.down = nextRow;
+				current = current.right;
+				prevRow = prevRow.right;
+				nextRow = nextRow.right;
+			}
+			
+			rows++;
+		}		 
 	}
-	
+
 	public void deleteFirstCol() {
 		// Removes the first column. 
 	}
-	
+
 	public void deleteFirstRow() {
 		// Removes the first row. 
 	}
-	
+
 	public void deleteLastCol() {
 		// Removes the last column. 
 	}
-	
+
 	public void deleteLastRow() {
 		// Removes the last row. 
 	}
-	
+
 	public void deleteCol(int index) {
 		// Removes the column at the given index. 
 	}
-	
+
 	public void deleteRow(int index) {
 		// Removes the row at the given index. get(row, col): Returns the item at the given (row, col).  NOTE: You must return the item stored in the Array2DNode, NOT the Array2DNode itself. 
 	}
-	
+
 	public void getCol(int col) {
 		// Returns the Array2DNode that starts the beginning of the requested column. 
 	}
-	
+
 	public void getRow(int row) {
 		// Returns the Array2DNode that starts the beginning of the requested row. 
 	}
-	
+
 	public void set(int row, int col, E item) {
 		// Assigns the given item to the Array2DNode at position (row, col). 
 	}
-	
-	public void colSize() {
-		// Returns the number of columns. 
+
+	public int colSize() {
+		return cols;
 	}
-	
-	public void rowSize() {
-		// Returns the number of rows. to2DArray(): Converts this linked Array2D object into a normal 2D array.  You may not use this function to implement other functions in your Array2D class.  This function must create a local 2D array and return this array. 
+
+	public int rowSize() {
+		return rows;
 	}
-	
+
 }
